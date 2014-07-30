6dd686ca9839e4cd6016a2f4aab0f3904d733757
Eugene Li <hello@itseugene.li>
2014-07-30 15:42:28 -0400

    Started PingService class

---
+++ app/src/com/ping/PingService.java
@@ -0,0 +1,80 @@
+ package com.ping;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Response;
+import com.ping.util.PingApi;
+import com.ping.util.PingPrefs;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+
+public class PingService extends Service
+{
+	private final String TAG = PingService.class.getSimpleName();
+	
+	private PingApi pingApi;
+	private PingPrefs prefs;
+	
+	@Override
+    public void onCreate()
+	{
+        super.onCreate();
+        prefs = PingPrefs.getInstance(this);
+        pingApi = PingApi.getInstance(this, prefs.getAuthToken());
+    }
+	
+	private void handleIntent(Intent intent)
+	{
+		Log.i(TAG, "Service started");
+		
+        // Check the global background data setting
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        if (!cm.getBackgroundDataSetting() || (cm.getActiveNetworkInfo() != null && !cm.getActiveNetworkInfo().isAvailable()))
+        {
+            stopSelf();
+            return;
+        }
+        
+		LatLng loc = prefs.getLocation();
+		int radius = prefs.getRadius();
+		
+		pingApi.getPingsInArea(loc.latitude, loc.longitude, radius, new FutureCallback<Response<JsonObject>>() {
+			@Override
+			public void onCompleted(Exception e, Response<JsonObject> response)
+			{
+				response.getResult().getAsJsonObject().get("response");
+				//TODO: Do something with the pings received
+			}
+		});
+		
+	    stopSelf();
+	}
+	
+	@Override
+    public void onStart(Intent intent, int startId) {
+        handleIntent(intent);
+    }
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId)
+	{
+		handleIntent(intent);
+	    return Service.START_NOT_STICKY;
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
